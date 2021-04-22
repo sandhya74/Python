e01e22e7731768ed622ef6eb538998ac71b34491
sandhya74 <sandhiya@5gindia.net>
2021-04-22 13:45:10 +0530

    update parkingtest.py

--- Parkingtest.py
+++ Parkingtest.py
@@ -1,43 +1,52 @@
 import unittest
-
+from unittest.mock import Mock
+from unittest.mock import patch
 from Vehicleparking import Vehicle_parking
 
 
 class Parking_Test(unittest.TestCase):
     def setUp(self):
-        self.result=Vehicle_parking()
+        self.res=Vehicle_parking()
         self.total_lot_space=5
-        self.vehicle_type='two wheeler'
-        self.vehicle_space=2
-#Positive Test case
+        self.vehicle_type=0
+        self.vehicle_space=0
+
     def test_parking(self):
-   
+        #import pdb;pdb.set_trace()
         user_input={self.vehicle_type:self.vehicle_space}
         expected_output={'two wheeler':2}
         available_space=3
-        #park=self.result.parking()
-        self.assertEqual(user_input == expected_output,True)
-        self.assertEqual(self.total_lot_space != available_space,True)
-# Negative Test case
-    def test_parking2(self):
-        user_input={self.vehicle_type:self.vehicle_space}
-        expected_output={'two wheeler':2}
-        available_space=3
-        #park=self.result.parking()
-        self.assertEqual(user_input!= expected_output,False)
-        self.assertEqual(self.total_lot_space == available_space,False)
-#positive Test Case
+        with patch('builtins.input', side_effect=user_input):
+            park=self.res
+            self.assertEqual(self.total_lot_space != available_space,True)
+
     def test_to_get_vehicle(self):
         actual_output={'two wheeler':1,'four wheeler':2,'three wheeler':3}
         expected_output={'two wheeler':1,'four wheeler':2}
-        #un_park=self.result.to_get_vehicle()
-        self.assertEqual(actual_output != expected_output,True)
-#Negative Test Case        
-    def test_to_get_vehicle2(self):
-        actual_output={'two wheeler':1,'four wheeler':2,'three wheeler':3}
-        expected_output={'two wheeler':1,'four wheeler':2}
-        #un_park=self.result.to_get_vehicle()
-        self.assertEqual(actual_output == expected_output,False)
+        available_space=2
+        with patch('builtins.input', side_effect=actual_output):
+            un_park=self.res
+            self.assertEqual(un_park != expected_output,True)
+            self.assertEqual(self.total_lot_space != available_space,True)
+
+    def test_calculate_space(self):
+        occuiped_space=3
+        
+        with patch('builtins.input', side_effect=occuiped_space):
+            available_space=self.res
+            self.assertEqual(self.total_lot_space - occuiped_space,2)
+            self.assertEqual(available_space == occuiped_space ,False)
+        
+    def test_view_status(self):
+        available_space=4
+        with patch('builtins.input',side_effect=available_space):
+            view=self.res                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+            self.assertEqual(available_space == self.total_lot_space ,False)
+            self.assertEqual(view != available_space,True)
+
+                                                                                                                                                  
+
+
 
 
     
