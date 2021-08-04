03cf5a2181b719012c702598425570b74176fcba
sandhya74 <sandhiya@5gindia.net>
2021-08-04 12:24:15 +0530

    Remove duplicate floder

--- myproject/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from blog.models import *
-
-class MaterialsForm(forms.ModelForm):
-    class Meta:
-        model=Materials
-        fields = '__all__'
